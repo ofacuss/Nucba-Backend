e6006f8ae3ccf8b7409156a9186393d52c456988
ofacuss <123487458+ofacuss@users.noreply.github.com>
2026-01-29 19:26:24 -0300

    Update API endpoint to fetch products

--- cliente/Paginas/Productos.jsx
+++ cliente/Paginas/Productos.jsx
@@ -17,7 +17,7 @@ const Productos = () => {
         const obtenerProductos = async () => {
             // Petición GET
             try {
-                const { data } = await api.get('https://nucba-backendserver.vercel.app/api');
+                const { data } = await api.get('https://nucba-backendserver.vercel.app/api/products');
                 // Guardamos en el estado
                 setListaProductos(data.productos);
             } catch (error) {
